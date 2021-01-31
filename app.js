b1086cdd1d18014cfeb4ba320fd47bb8b534f4fc
Arnar Ingi Njarðarson <arnarnjardarson@gmail.com>
2021-01-31 16:19:28 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,54 @@
+const path = require('path');
+const express = require('express');
+
+const videos = require('./src/videos');
+
+const app = express();
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', videos);
+
+
+
+/**
+ * Middleware sem sér um 404 villur.
+ *
+ * @param {object} req Request hlutur
+ * @param {object} res Response hlutur
+ * @param {function} next Næsta middleware
+ */
+function notFoundHandler(req, res, next) { // eslint-disable-line
+  const title = 'Síða fannst ekki';
+  const message = 'Efnið sem þú sóttist eftir er ekki til';
+  res.status(404).render('error', { title, message });
+}
+
+/**
+ * Middleware sem sér um villumeðhöndlun.
+ *
+ * @param {object} err Villa sem kom upp
+ * @param {object} req Request hlutur
+ * @param {object} res Response hlutur
+ * @param {function} next Næsta middleware
+ */
+function errorHandler(err, req, res, next) { // eslint-disable-line
+  console.error(err);
+  const title = 'Villa kom upp';
+  const message = '';
+  res.status(500).render('error', { title, message });
+}
+
+app.use(notFoundHandler);
+app.use(errorHandler);
+
+//Útbúum breytur fyrir hostname og port og skilgreinum þær 
+const hostname = '127.0.0.1';
+const port = 3000;
+
+//Prentum upplýsingarnar um hvar verkefnið sé keyrt
+app.listen(port, hostname, () => {
+  console.info(`Server running at http://${hostname}:${port}/`);
+});
---
+++ src/videos.js
@@ -0,0 +1,75 @@
+const util = require('util');
+const fs = require('fs');
+const express = require('express');
+
+const router = express.Router();
+
+const readFileAsync = util.promisify(fs.readFile);
+
+/**
+ * Higher-order fall sem umlykur async middleware með villumeðhöndlun.
+ *
+ * @param {function} fn Middleware sem grípa á villur fyrir
+ * @returns {function} Middleware með villumeðhöndlun
+ */
+function catchErrors(fn) {
+    return (req, res, next) => fn(req, res, next).catch(next);
+  }
+
+/**
+ * Les gögn async úr JSON skrá.
+ *
+ * @returns {object} Gögnum úr JSON skrá
+ */
+async function readList(){
+    // Byrjum á að lesa skjalið (videos.json) sem staðsett er í möppunni fyrir neðan 
+    const file = await readFileAsync('./videos.json');
+    // Notum JSON til vinna skrárnar
+    const json = JSON.parse(file);
+    // Skilum gögnunum úr JSON skránni (videos.json)
+    return json;
+}
+
+/**
+ * Route handler sem birtir lista af myndböndum.
+ *
+ * @param {object} req Request hlutur
+ * @param {object} res Response hlutur
+ */
+async function list(req, res) {
+    const title = 'Fræðslumyndbandaleigan';
+    const json = await readList();
+    const { videos } = json;
+  
+    res.render('videos', { title, videos });
+  }
+
+/**
+ * Route handler sem birtir myndband. Ef myndband finnst ekki í JSON skrá
+ * er kallað í next() sem mun enda í 404 handler.
+ *
+ * @param {object} req Request hlutur
+ * @param {object} res Response hlutur
+ * @param {function} next Næsta middleware
+ */
+async function video(req, res, next) {
+    const { slug } = req.params;
+  
+    const json = await readList();
+    const { videos } = json;
+  
+    const foundVideo = videos.find(a => a.slug === slug);
+  
+    if (!foundVideo) {
+      // Sendum í 404 handler
+      return next();
+    }
+  
+    const { title } = foundVideo;
+  
+    return res.render('video', { title, video: foundVideo });
+}
+router.get('/', catchErrors(list));
+router.get('/:slug', catchErrors(video));
+
+module.exports = router;
